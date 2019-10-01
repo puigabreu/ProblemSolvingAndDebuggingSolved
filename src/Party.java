c88dab495fe70858dbe2bd803aef61431c4bb902
Fatima Puig <fatima.puig@upr.edu>
2019-10-01 17:37:12 -0400

    Updated solution to include exercises

--- src/Party.java
+++ src/Party.java
@@ -1,11 +1,11 @@
 public class Party {
-	
+
 	private int capacity;
 	private int guestNumber;
 	private String[] guests;
 	private int timeElapsed;
-	
-	
+
+
 	// A constructor for a Party. Assumes that guests.length = capacity and that
 	// there are no null entries. Uses copy method
 	public Party(String[] guests) {
@@ -14,27 +14,27 @@ public class Party {
 		this.timeElapsed = 0;
 		this.guestNumber = capacity;
 	}
-	
+
 	// Causes an hour to pass in the party. Each hour, a guest leaves.
 	// Once half the capacity of hours have passed, everyone leaves.
 	public void timePasses() {
 		this.timeElapsed++;
 		guestNumber--;
 		guests[guestNumber] = null;
-		
+
 		if(this.timeElapsed > capacity/2) {
 			this.guests = new String[capacity]; 
 			this.guestNumber = 0;
 		}
 	}
-	
+
 	// Helper method to print array
 	public static void printArray(String[] arr) {
 		for(int i = 0; i < arr.length && arr[i] != null; i++)
 			System.out.print(arr[i] + " ");
 		System.out.println();
 	}
-	
+
 	// Implement a static method to create a deep copy of the array so the
 	// array given in the constructor isn't affected
 	public static String[] copy(String[] guests) {
@@ -43,7 +43,26 @@ public class Party {
 			finalArray[i] = guests[i];
 		return finalArray;
 	}
-	
+
+	//Returns true if the guest is in the party
+	public boolean containsGuest(String guest) {
+		for(String presentGuest : this.guests)
+			if(presentGuest.equals(guest))
+				return true;
+		return false;
+	}
+
+	// If there is space in the party, add the guest
+	public void addGuest(String guest) {
+		if(guestNumber < this.guests.length)
+			this.guests[guestNumber++] = guest;
+	}
+
+	// Returns true if there are no guests
+	public boolean isOver() {
+		return guestNumber == 0;
+	}
+
 	public static void main(String[] args) {
 		String[] guests = {"Maria", "Jorge", "Fatima", "Orlando", "Carola", "Alex"};
 		printArray(guests);
